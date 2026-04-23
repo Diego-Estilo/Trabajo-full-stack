003feb088b69da50bc136622483b99460d22c5c2
Diego-Estilo <diegoestilo23@gmail.com>
2026-04-22 21:51:42 -0500

    feature:para despliegue de pagina mejoras

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  // Descomentar y modificar si tu repo no es el principal:
+basePath: '/Trabajo-full-stack',
 }
 
 module.exports = nextConfig
