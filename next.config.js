0ba0dd6969f2fed0d7885142f86f72c7fe6ff187
Alfredo Gonzales <ramzvaal@gmail.com>
2026-04-24 00:32:27 -0500

    Corrección de next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  // Descomentar y modificar si tu repo no es el principal:
-basePath: '/Trabajo-full-stack',
 }
 
 module.exports = nextConfig
